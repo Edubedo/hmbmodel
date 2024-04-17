82c3b97382ecd61ee89982aa25b894d52515e05c
eescobedo <ezescobedo27@gmail.com>
2024-04-17 00:07:07 -0600

    Boton de cerrar sesion

--- app/src/main/java/com/example/splash/PaginaLogin.java
+++ app/src/main/java/com/example/splash/PaginaLogin.java
@@ -1,24 +1,43 @@
 package com.example.splash;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class PaginaLogin extends AppCompatActivity {
+
+    private Button btnLogout;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pagina_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnLogout = findViewById(R.id.btnLogout);
+
+        // Obtener SharedPreferences
+        sharedPreferences = getSharedPreferences("login", Context.MODE_PRIVATE);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Eliminar las credenciales almacenadas
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.remove("username");
+                editor.remove("password");
+                editor.apply();
+
+                // Redirigir a la actividad MainActivity
+                Intent intent = new Intent(PaginaLogin.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
         });
     }
 }
