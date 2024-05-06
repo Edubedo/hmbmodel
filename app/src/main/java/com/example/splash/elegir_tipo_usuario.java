37c642322bd2782fde398979480cfa3fec4fc06e
Caesar Canales <cesarfernando171@gmail.com>
2024-05-06 11:10:21 -0600

    cambios de fondo incompletos

--- app/src/main/java/com/example/splash/elegir_tipo_usuario.java
+++ app/src/main/java/com/example/splash/elegir_tipo_usuario.java
@@ -1,9 +1,12 @@
 package com.example.splash;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -46,5 +49,23 @@ public class elegir_tipo_usuario extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(20000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float height = backgroundOne.getHeight();
+                final float translationY = height * progress;
+                backgroundOne.setTranslationY(translationY);
+                backgroundTwo.setTranslationY(translationY - height);
+            }
+        });
+        animator.start();
     }
 }
