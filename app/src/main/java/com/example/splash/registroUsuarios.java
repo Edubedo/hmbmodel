75a899baa71cac4f7568022efd9fced4a74cf223
eescobedo <ezescobedo27@gmail.com>
2024-05-07 00:40:28 -0600

    Cambios

--- app/src/main/java/com/example/splash/registroUsuarios.java
+++ app/src/main/java/com/example/splash/registroUsuarios.java
@@ -15,7 +15,7 @@ public class registroUsuarios extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro_usuarios);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnLogin), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
