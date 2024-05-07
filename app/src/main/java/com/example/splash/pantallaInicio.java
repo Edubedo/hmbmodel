9137eb3ac370dc5f3b29368c49ceedc37303f67b
eescobedo <ezescobedo27@gmail.com>
2024-05-06 23:50:54 -0600

    Reestructura

--- app/src/main/java/com/example/splash/pantallaInicio.java
+++ app/src/main/java/com/example/splash/pantallaInicio.java
@@ -7,12 +7,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class pantallaInicio extends AppCompatActivity {
 
-    private Button btnLinkRegistro;
+    private Button btnLinkRegistro, btnLinkLogin;
     private SharedPreferences sharedPreferences;
 
     @SuppressLint("MissingInflatedId")
@@ -22,6 +23,22 @@ public class pantallaInicio extends AppCompatActivity {
         setContentView(R.layout.activity_pantalla_inicio);
 
         btnLinkRegistro = findViewById(R.id.btnLinkRegistro);
+        btnLinkLogin = findViewById(R.id.btnLinkLogin);
+        btnLinkRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    startActivity(new Intent(pantallaInicio.this, registroUsuarios.class));
+                    finish();
+            }
+        });
+
+        btnLinkLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(pantallaInicio.this, loginUsuarios.class));
+                finish();
+            }
+        });
 
 
 
