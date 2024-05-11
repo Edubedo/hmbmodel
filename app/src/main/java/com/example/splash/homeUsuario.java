7f9da0d88f64f1d40316413ba6d76b3ee73da4da
Lomeli <maya02manzanillo@gmail.com>
2024-05-11 17:19:36 -0600

    barra

--- app/src/main/java/com/example/splash/homeUsuario.java
+++ app/src/main/java/com/example/splash/homeUsuario.java
@@ -1,8 +1,12 @@
 package com.example.splash;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.view.animation.LinearInterpolator;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,5 +24,24 @@ public class homeUsuario extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        final ImageView fondo1 = (ImageView) findViewById(R.id.fondo1);
+        final ImageView fondo2 = (ImageView) findViewById(R.id.fondo2);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(@NonNull ValueAnimator animation) {
+                final float progress=(float) animation.getAnimatedValue();
+                final float width=fondo1.getWidth();
+                final float translationX=width*progress;
+                fondo1.setTranslationX(translationX);
+                fondo2.setTranslationX(translationX-width);
+            }
+        });
+        animator.start();
     }
 }
