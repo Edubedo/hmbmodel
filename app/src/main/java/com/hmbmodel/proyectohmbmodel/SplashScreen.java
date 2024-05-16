d7d70ed19493cf1952545db5c3a6397dcd01a127
eescobedo <ezescobedo27@gmail.com>
2024-05-15 22:52:28 -0600

    Splash screen

--- app/src/main/java/com/hmbmodel/proyectohmbmodel/SplashScreen.java
+++ app/src/main/java/com/hmbmodel/proyectohmbmodel/SplashScreen.java
@@ -1,24 +1,27 @@
 package com.hmbmodel.proyectohmbmodel;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+@SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, SplashScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
     }
 }
