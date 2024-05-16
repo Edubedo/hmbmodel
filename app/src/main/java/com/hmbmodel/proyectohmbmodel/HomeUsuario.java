30ededb0c1740d52d357f3202b2406dca95915c7
eescobedo <ezescobedo27@gmail.com>
2024-05-16 00:00:13 -0600

    Pantalla de inicio de usuarios

--- app/src/main/java/com/hmbmodel/proyectohmbmodel/HomeUsuario.java
+++ app/src/main/java/com/hmbmodel/proyectohmbmodel/HomeUsuario.java
@@ -1,8 +1,9 @@
 package com.hmbmodel.proyectohmbmodel;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +11,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeUsuario extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_usuario);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    // Método para manejar el clic en "Administrar gimnasios"
+    public void redirectToAdministrarGimnasios(View view) {
+        Intent intent = new Intent(this, MiCuenta.class);
+        startActivity(intent);
+    }
+
+    // Método para manejar el clic en "Mi cuenta"
+    public void redirectToMiCuenta(View view) {
+        Intent intent = new Intent(this, MiCuenta.class);
+        startActivity(intent);
     }
 }
