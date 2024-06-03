31ecd35e705f09ca110b67fb609abaa77813611d
eescobedo <ezescobedo27@gmail.com>
2024-06-02 21:05:28 -0600

    Cambios

--- app/src/main/java/com/hmbmodel/proyectohmbmodel/UsuarioAdapter.java
+++ app/src/main/java/com/hmbmodel/proyectohmbmodel/UsuarioAdapter.java
@@ -16,6 +16,10 @@ public class UsuarioAdapter extends RecyclerView.Adapter<UsuarioAdapter.UsuarioV
     public UsuarioAdapter(List<Usuario> usuarios) {
         this.usuarios = usuarios;
     }
+    public void actualizarListaUsuarios(List<Usuario> nuevosUsuarios) {
+        this.usuarios = nuevosUsuarios;
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
